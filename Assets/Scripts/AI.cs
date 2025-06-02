4e308b64f55b2e2dac06d274a278a7009a3a41b0
Dani-Mol <78124826+Dani-Mol@users.noreply.github.com>
2025-06-02 09:42:16 -0600

    Creacion de enemigo con IA (patron de recorrido)

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public enum MoveMode
+{
+    patrol, chase, wait
+}
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+
+public class AI : MonoBehaviour
+{
+    public MoveMode moveMode;
+
+    [Header("Steering")]
+    public float patrolSpeed;
+
+    [Header("Transform")]
+    public Transform[] patrolPoint;
+    public NavMeshAgent agent;
+
+    Vector3 destination;
+    int indexPatrolPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (agent == null)
+            agent = GetComponent<NavMeshAgent>();
+
+        if (agent.stoppingDistance < 0.5f)
+            agent.stoppingDistance = 0.5f;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (moveMode)
+        {
+            case MoveMode.patrol:
+                Patroling();
+                break;
+            case MoveMode.chase:
+
+                break;
+
+            case MoveMode.wait:
+
+                break;
+        }
+    }
+
+    void Patroling()
+    {
+        agent.speed = patrolSpeed;
+        agent.destination = destination = patrolPoint[indexPatrolPoint].position;
+
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            indexPatrolPoint = Random.Range(0, patrolPoint.Length);
+            Debug.Log("Change patrol to " + indexPatrolPoint.ToString());
+        }
+    }
+}
